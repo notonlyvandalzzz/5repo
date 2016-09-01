2c048a67f089771a9dac6390d12fb476921db28b
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-01 16:37:11 +1000

    F: C and V mod

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,8 @@
 class FeedbacksController < ApplicationController
+  def new
+  end
+
+  def create
+    render plain: params[:feedback].inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get '/contacts' => 'feedback#index'
-  post '/contacts' => 'feedback#create'
+  # get '/contacts' => 'feedback#index'
+  # post '/contacts' => 'feedback#create'
   get '/' => 'home#index'
 
+  resource :feedbacks, only: [:new, :create]
   resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
