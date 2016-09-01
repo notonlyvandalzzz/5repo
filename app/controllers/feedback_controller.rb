39d15bd161c4851634cad0eac95dffa8803204a0
notonlyvandalzzz <abs.deranger@gmail.com>
2016-09-01 16:33:38 +1000

    controller remake

--- app/controllers/feedback_controller.rb
+++
@@ -1,8 +0,0 @@
-class FeedbackController < ApplicationController
-  def index
-  end
-
-  def create
-    render plain: params[:feedback].inspect
-  end
-end
--- test/controllers/feedback_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FeedbackControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get feedback_index_url
-    assert_response :success
-  end
-
-end
