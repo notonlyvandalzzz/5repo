658d80f76a3663fc95e0cf76637608e7d79112e8
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-06 16:15:20 +1000

    comments#create add

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @artcl_curr = Article.find(params[:article_id])
+    @artcl_curr.comments.create(comment_params)
+    redirect_to @artcl_curr
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :body) 
+  end
 end
