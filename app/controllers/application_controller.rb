ccfcb38ac5320fb25796d60ddbd416ea90f21924
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-09 14:28:42 +1000

    1,3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up) << :username
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me])
      # devise_parameter_sanitizer.for(:sign_in) << :username
      # devise_parameter_sanitizer.for(:account_update) << :username
   end 
