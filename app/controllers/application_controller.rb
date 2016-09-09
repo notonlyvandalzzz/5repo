cd7429dd9ebe50931a5d16e41ad92052c3200883
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-09 14:18:02 +1000

    1.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-protected
+  protected
 
-def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) << :username
-   # devise_parameter_sanitizer.for(:sign_in) << :username
-   # devise_parameter_sanitizer.for(:account_update) << :username
-end 
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_up) << :username
+     # devise_parameter_sanitizer.for(:sign_in) << :username
+     # devise_parameter_sanitizer.for(:account_update) << :username
+  end 
 end
