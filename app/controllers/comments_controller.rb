d166b6b44ea1fa8dd0d08ecd65cbbd08f29c5f50
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-12 16:18:12 +1000

    screw it

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @artcl_curr = Article.find(params[:article_id])
-    if User.find_by username: comment_params[:comment][:author] && !user_signed_in?
+    # if User.find_by username: comment_params[:comment][:author] && !user_signed_in?
       @artcl_curr.comments.create(comment_params)
       redirect_to @artcl_curr
-    else
-      redirect_to articles_url
-    end
+    # else
+    #   redirect_to articles_url
+    # end
   end
 
 
