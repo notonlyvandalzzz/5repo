5b14174a51d6d8b16f848b30f5dac104e34c9b0b
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-13 15:51:51 +1000

    2nd test with FG

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -18,4 +18,14 @@ describe Article do
     end
   end
 
+  describe "#last_comment" do
+    it "returns last comment" do
+      tmpuser = create(:user, id: 1, username: 'John', email: '123@45.com', password: '123444332111')
+      article = create(:article, title: 'Hello world', text: 'Some text', user: User.find(1))
+      comm1 = create(:comment, author: 'one', body: 'oneone', article: Article.last)
+      comm2 = create(:comment, author: 'two', body: 'twotwo', article: Article.last)
+      expect(article.last_comment.body).to eq 'twotwo' 
+    end
+  end
+
 end
