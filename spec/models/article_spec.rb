698720629bebe6c9331163b90b61e27660b07325
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-13 14:13:59 +1000

    2nd test mod

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe Article do 
-
-  it { should have_many(:comments) }
-  it { should belong_to(:user) }
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:text) }
+  end
+  describe "associations" do
+    it { should have_many(:comments) }
+    it { should belong_to(:user) }
+  end
 
 
 end
