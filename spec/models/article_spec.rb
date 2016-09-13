7658f65ff2cfbacd4a0d71eea9b0d8d8e91e1a26
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-13 15:28:12 +1000

    1.3

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,7 +12,7 @@ describe Article do
 
   describe "#subject" do
     it "returns article title" do
-      article = create(:article, title: 'Hello world', text: 'Some text', user: 1)
+      article = create(:article, title: 'Hello world', text: 'Some text', user_id: 1)
       expect(article.subject).to eq 'Hello world'
     end
   end
