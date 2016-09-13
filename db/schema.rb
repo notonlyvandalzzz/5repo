57dc2b30dff4fbd03eb90c5d0ded620209b2debb
notonlyvandalzzz <abs.deranger@gmail.com>
2016-09-13 11:14:07 +1000

    migr perform

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913003724) do
+ActiveRecord::Schema.define(version: 20160913010117) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160913003724) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
