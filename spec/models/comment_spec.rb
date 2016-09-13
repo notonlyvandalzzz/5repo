e4e44595d8c249ac1e01f5ee73aa6df513029840
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-13 16:24:24 +1000

    comment validations test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,5 +3,10 @@ require 'spec_helper'
 describe Comment do 
 
   it { should belong_to(:article) }
+  it do 
+      should validate_length_of(:body).
+      is_at_least(1).is_at_most(140)
+  end 
+ 
   
 end
