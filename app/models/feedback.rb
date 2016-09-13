36d36bb4c3825a505cabc92ae1d94671fe0a2fe6
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-13 13:42:25 +1000

    rspec fun

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,4 @@
 class Feedback < ApplicationRecord
-  validates :email, presence: true
+ # validates :email, presence: true
   validates :content, presence: true, length: { minimum: 5, too_short: "Message must be minimum 5chr" }
 end
