004c34dff7ff83edd3dd4c52f472008d13701b4d
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-21 17:16:53 +1000

    User reg test

---
+++ spec/features/visitor_create_account_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature "Account creation" do 
+  scenario "allows guest to create account" do
+    visit new_user_registration_path
+
+    fill_in :user_email, :with => 'user@example.com'
+    fill_in :user_username, :with => 'user_name_test'
+    fill_in :user_password, :with => '123456ttt'
+    fill_in :user_password_confirmation, :with => '123456ttt'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content 'success'
+  end
+end
