87c9b51ef42349b36ae2f736e0059fdc03b1f29d
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-21 18:04:10 +1000

    session_helper create

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -8,13 +8,3 @@ feature "Account creation" do
   end
 end
 
-def sign_up
-    visit new_user_registration_path
-
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'user_name_test'
-    fill_in :user_password, :with => '123456ttt'
-    fill_in :user_password_confirmation, :with => '123456ttt'
-
-    click_button 'Sign up'
-end
