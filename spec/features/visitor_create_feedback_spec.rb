2ae551ade868ab8d23b42a15b0cd1a1978bd4ab0
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-21 14:30:32 +1000

    1.1

--- spec/features/visitor_create_feedback_spec.rb
+++ spec/features/visitor_create_feedback_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
-feature "..." do 
-  scenario ".." do
+feature "Feedback creation" do 
+  scenario "allows access to feedback page" do
+    visit '/feedbacks'
 
+    expect(page).to have_content 'Thanks!'
   end
 end
