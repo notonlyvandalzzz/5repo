bb9f0fc8b661601b649595a71abfbb830aa5794a
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-22 13:30:20 +1000

    1.4

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -28,8 +28,8 @@ describe Article do
 
   describe "#last_comment" do
     it "returns last comment" do
-      tmpuser = create(:user, id: 1, username: 'John', email: '123@45.com', password: '123444332111')
-      article = create(:article, title: 'Hello world', text: 'Some text', user: User.find(1))
+      tmpuser = create(:user, id: 2, username: 'John2', email: '123@456.com', password: '123444332111')
+      article = create(:article, title: 'Hello world', text: 'Some text', user: User.find(2))
       comm1 = create(:comment, author: 'one', body: 'oneone', article: Article.last)
       comm2 = create(:comment, author: 'two', body: 'twotwo', article: Article.last)
       expect(article.last_comment.body).to eq 'twotwo' 
