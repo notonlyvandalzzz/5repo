3e5548e4ae5e0220ffb5065e9aed4db50cd7945d
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-22 11:33:15 +1000

    session_helper mod

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,7 +1,7 @@
 def sign_up
     visit new_user_registration_path
 
-    fill_in :user_email, :with => 'user@example.com'
+    fill_in :user_email, :with => 'user33@example.com'
     fill_in :user_username, :with => 'user_name_test'
     fill_in :user_password, :with => '123456ttt'
     fill_in :user_password_confirmation, :with => '123456ttt'
