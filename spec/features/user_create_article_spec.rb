e7c7d8ed5f7ca433f612d1870ef410e03655363f
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-22 14:26:44 +1000

    user creates article and leaves comment test add

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -19,5 +19,15 @@ feature "Article creation" do
     click_button 'Save Article'
     expect(page).to have_content ('test_article_title')
   end
+
+  scenario "allows user to post article and leave comment" do
+    visit new_article_path
+    fill_in :article_title, :with => 'test_article_title'
+    fill_in :article_text, :with => 'test_article_text'
+    click_button 'Save Article'
+    fill_in :comment_body, :with => 'test_comment_text'
+    click_button 'Send'
+    expect(page).to have_content ('test_comment_text')
+  end
 end
 
