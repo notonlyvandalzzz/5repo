d7e62c0b969e852a78b67cf0881e2f91ed4db457
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-22 13:27:53 +1000

    1.3

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 feature "Account creation" do 
-  scenario "allows guest to create account" do
+  before (:all) do
     sign_up
+  end
+  
+  scenario "allows guest to create account" do
+    
 
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
